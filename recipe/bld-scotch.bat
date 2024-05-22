2a39978e8ffc57207ac6be5c21d905623cf49d6e
Min RK <benjaminrk@gmail.com>
2024-05-22 20:04:35 +0200

    deal with empty include/mpi
    not sure why FindMPI does that

--- recipe/bld-scotch.bat
+++ recipe/bld-scotch.bat
@@ -4,12 +4,15 @@ set BISON_PKGDATADIR=%BUILD_PREFIX%\Library\share\winflexbison\data\
 set CC=cl.exe
 set CXX=cl.exe
 
+:: IMPI doesn't make the include\mpi dir, but cmake FindMPI assumes it's there
+if not exist "%LIBRARY_PREFIX%\include\mpi" mkdir "%LIBRARY_PREFIX%\include\mpi"
+
 cmake ^
+  --trace ^
   -G "Ninja" ^
   -D CMAKE_BUILD_TYPE=Release ^
   -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
   -D BUILD_SHARED_LIBS=OFF ^
-  -D MPI_C_INCLUDE_DIRS=%LIBRARY_PREFIX%\include ^
   -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
   -D THREADS_PTHREADS_INCLUDE_DIR="%LIBRARY_INC%" ^
   -D THREADS_PTHREADS_WIN32_LIBRARY:FILEPATH="%LIBRARY_LIB%\pthread.lib" ^
@@ -21,3 +24,6 @@ cmake --build ./build --config Release
 if errorlevel 1 exit 1
 cmake --install ./build --component=libscotch
 if errorlevel 1 exit 1
+
+:: remove empty directory (ignore errorlevel)
+rmdir "%LIBRARY_PREFIX%\include\mpi"
